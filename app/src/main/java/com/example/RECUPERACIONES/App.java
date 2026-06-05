15d5d953d36279ca2b722c0fe3f288cc5addfb77
NoemiGuapi <noemi.guapi0393@utc.edu.ec>
2026-06-04 22:01:58 -0500

    Comit 8: Modificado encabezado de resultados

--- app/src/main/java/com/example/RECUPERACIONES/App.java
+++ app/src/main/java/com/example/RECUPERACIONES/App.java
@@ -8,14 +8,14 @@ public class App {
         System.out.println(" ¡Hola Noemi! Iniciando calculadora...  ");
         System.out.println("     CÁLCULO DE LA HIPOTENUSA            ");
         System.out.println("*****************************************");
-        System.out.println("-----------------------------------------"); // <-- NUEVO ADORNO
+        System.out.println("-----------------------------------------");
         System.out.print("Por favor, digite el primer Cateto (A): ");
         double catetoA = teclado.nextDouble();
         System.out.print("Por favor, digite el segundo Cateto (B): ");
         double catetoB = teclado.nextDouble();
         double hipotenusa = Math.sqrt(Math.pow(catetoA, 2) + Math.pow(catetoB, 2));
         System.out.println("-----------------------------------------");
-        System.out.println("RESULTADOS:");
+        System.out.println("=== IMPRIMIENDO RESULTADOS FINALES ==="); // <-- TEXTO CAMBIADO
         System.out.println("-> El valor de la Hipotenusa es: " + hipotenusa);
         System.out.println("=========================================");
         System.out.println("Cálculo terminado con éxito. ¡Adiós!");
