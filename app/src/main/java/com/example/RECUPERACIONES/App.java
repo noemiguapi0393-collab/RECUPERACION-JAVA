641168b3ef6d456f88dcc59b16b430ef04883910
NoemiGuapi <noemi.guapi0393@utc.edu.ec>
2026-06-04 22:11:37 -0500

    Comit 13:Modificado el titulo del menu

--- app/src/main/java/com/example/RECUPERACIONES/App.java
+++ app/src/main/java/com/example/RECUPERACIONES/App.java
@@ -7,24 +7,19 @@ public class App {
         Scanner teclado = new Scanner(System.in);
         System.out.println("*****************************************");
         System.out.println(" ¡Hola Noemi! Iniciando calculadora...  ");
-        System.out.println("     CÁLCULO DE LA HIPOTENUSA            ");
+        System.out.println("     TEOREMA DE PITÁGORAS (SISTEMA)      "); // <-- TITULO MODIFICADO
         System.out.println("*****************************************");
         System.out.println("-----------------------------------------");
         System.out.print("Por favor, digite el primer Cateto (A): ");
         double catetoA = teclado.nextDouble();
         System.out.print("Por favor, digite el segundo Cateto (B): ");
         double catetoB = teclado.nextDouble();
-        
-        // Aplicamos la formula matematica del Teorema de Pitagoras
         double hipotenusa = Math.sqrt(Math.pow(catetoA, 2) + Math.pow(catetoB, 2));
-        
         System.out.println("-----------------------------------------");
         System.out.println("=== IMPRIMIENDO RESULTADOS FINALES ===");
         System.out.println("* Hipotenusa obtenida: " + hipotenusa);
         System.out.println("=========================================");
         System.out.println("Cálculo terminado con éxito. ¡Adiós!");
-        
-        // Cerramos el objeto scanner para liberar memoria
-        teclado.close(); // <-- COMENTARIO NUEVO ARRIBA
+        teclado.close();
     }
 }
